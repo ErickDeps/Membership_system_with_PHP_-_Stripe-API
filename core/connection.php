c35d9d765b25ad5f8a1f3ef240d45b0d748ab8b7
ErickDeps <erickdepss@gmail.com>
2025-08-06 23:45:25 -0600

    Second commit

--- core/connection.php
+++ core/connection.php
@@ -1,8 +1,8 @@
 <?php
-$host = $_ENV['DB_HOST'] ?? '127.0.0.1';
-$db = $_ENV['DB_DATABASE'] ?? 'membership_db';
-$user = $_ENV['DB_USERNAME'] ?? 'root';
-$pass = $_ENV['DB_PASSWORD'] ?? '';
+$host = $_ENV['DB_HOST'];
+$db = $_ENV['DB_DATABASE'];
+$user = $_ENV['DB_USERNAME'];
+$pass = $_ENV['DB_PASSWORD'];
 try {
     $connection = new PDO('mysql:host=' . $host . ';' . 'dbname=' . $db, $user, $pass);
 } catch (PDOException $e) {
